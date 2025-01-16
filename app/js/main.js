31a1ec63dcf5b60bf6ebcbe9d85329fbcb88b7db
BobbyBubovski <bobbybubovski@gmail.com>
2025-01-16 17:33:53 +0200

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,23 @@
 $(function(){
+    console.log('Инициализация началась');
+
     $('.top-slider__inner').slick({
         dots:true,
         arrows: false,
         fade: true,
         autoplay: true,
         autoplaySpeed: 2000
-    })
+    });
+
+    console.log('Slick-Slider инициализирован');
+
+    $('.star').rateYo({
+        rating: 3.5,
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+    });
+
+    console.log('RateYo инициализирован');
 });
