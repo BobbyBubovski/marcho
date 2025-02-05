571f0d3bbc0d8e587c96136a3bae81d112764591
BobbyBubovski <bobbybubovski@gmail.com>
2025-02-05 05:50:52 +0200

    create a shop page

--- gulpfile.js
+++ gulpfile.js
@@ -126,6 +126,8 @@ function scripts() {
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         'node_modules/@rateyo/jquery/lib/iife/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js', 
         'app/js/main.js'
     ]) // Указываем исходный JS файл
         .pipe(newer('app/js/main.min.js')) // Проверяем, изменился ли файл
